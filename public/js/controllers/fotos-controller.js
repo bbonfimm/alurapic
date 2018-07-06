a1f16814bb5858fc61f10fdb284469d20eb227be
Bruno Bonfim <brunomagbonfim@gmail.com>
2018-07-05 23:41:52 -0300

    vaiiii

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,16 +1,18 @@
 angular.
     module('alurapic').
-        controller('FotosController', function ($scope, $http) {
+    controller('FotosController', function ($scope, $http) {
 
-    $scope.fotos = [];
-    $http.get('v1/fotos')
-        .success(function (fotos) {
-            $scope.fotos = fotos;
-        })
-        .error(function (erro) {
-            console.log(erro);
-        });
-});
+        $scope.fotos = [];
+        $scope.filtro = '';
+
+        $http.get('v1/fotos')
+            .success(function (fotos) {
+                $scope.fotos = fotos;
+            })
+            .error(function (erro) {
+                console.log(erro);
+            });
+    });
 
 // Exemplo anterior de como foi feito Promise.
 //     var promise = $http.get ('v1/fotos');
